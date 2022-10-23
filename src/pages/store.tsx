e1eef17b935573381616afa0a80b47ee12362f84
Kade Illian <kadeillianmt@gmail.com>
2022-10-22 19:09:49 -0600

    added store item functionality and card.  Read .json data from the items.json in the data directory.  Also made a currency formatter

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -1,5 +1,13 @@
+import { Col, Row } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
+import StoreItems from "../data/items.json"
+
 export function Store() {
   return (
-    <h1>Store</h1>
+    <><h1>Store</h1>
+    <Row md={2} xs={1} lg={3} className="g-3">
+      {StoreItems.map(item => ( <Col key={item.id}><StoreItem {...item} /></Col>))}
+    </Row>
+    </>
   )
 }
