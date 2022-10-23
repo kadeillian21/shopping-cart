f8eb291d69e26689cd7d4ff8b3ebea9118df37da
Kade Illian <kadeillianmt@gmail.com>
2022-10-22 18:29:14 -0600

    finished sticky navbar and added items.json for out store data

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import {Container, Button, Nav, Navbar as NavbarBs} from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 
 export function Navbar() {
-  return <NavbarBs>
-    <Container className="bg-white shadow-sm mb-3">
+  return <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+    <Container>
       <Nav className="me-auto">
         <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
         <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
